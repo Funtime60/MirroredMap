27735d027b4882ebbf6be8bc39020212c608634c
Funtime60 <Funtime60@users.noreply.github.com>
2025-09-22 18:17:44 -0600

    Update mirroredMap.js

--- mirroredMap.js
+++ mirroredMap.js
@@ -1,80 +1,144 @@
-class MirroredMap extends Map {										// Extends the built-in Map class
-	get Class() { return MirroredMap; }								// Returns the class itself
+class MirroredMap extends Map {
+	get Class() { return MirroredMap; }
 
-	static #privateSymbol = Symbol('MirroredMap');					// Private symbol for internal use, it MUST remain internal to the class.
-	static #mirrorMapCheck = Symbol('MirroredMapCheck');			// Private check symbol storage, but the symbol is public.
-	static get symbol() { return this.#mirrorMapCheck; }			// Static getter for the check symbol. This way the symbol is RO
-	#proxies = [];													// Private array to store proxy instances.
-	get mirror() { return this.#proxies.at(-1); }					// Returns the last proxy in the array.
-	set mirror(proxy) {												// Setter for the mirror property.
-		if(proxy === undefined) return this.#proxies.length > 1 ? this.#proxies.pop() : undefined;
-																	// Removes last proxy if undefined.
-		if(proxy[this.Class.#privateSymbol] !== this || proxy[Symbol()] === this) throw new Error("Proxy cannot be confirmed to reference the correct backer.");
-																	// Validates the proxy.
-		this.#proxies.push(proxy);									// Adds the proxy to the array.
+	
+	static #privateSymbol = Symbol('MirroredMap'); // Private symbol for internal use, it MUST remain internal to the class.
+	static #mirrorMapCheck = Symbol('MirroredMapCheck'); // Private check symbol storage, but the symbol is public.
+	static get publicSymbol() { return this.#mirrorMapCheck; }
+	static #symbolList = [this.#privateSymbol, this.#mirrorMapCheck]; // Array of symbols since they're special.
+
+	static #getAllKeys(obj) {	// Recursive helper function to get the names of all the gettable keys on an object.
+		const prototype = Object.getPrototypeOf(obj);
+		const inherited = (prototype) ? this.#getAllKeys(prototype) : [];
+		return [...new Set(Object.getOwnPropertyNames(obj).concat(inherited))];
 	}
 
-	constructor(iterable) {											// Constructor accepts an iterable.
-		super(iterable);											// Calls Map constructor.
-		this.mirror = new Proxy(this, {								// Creates a proxy for this instance.
-			defineProperty: (_, __, ___) => false,					// Disables defineProperty, I don't use it internally and don't use it externally personally so I have no clue how to implement it. Disabling it doesn't seem to break anything.
-			deleteProperty: (map, property) => !(property === map.Class.symbol || property === map.Class.#privateSymbol) && (map.#super('delete', property) || true),
-																	// If the property isn't a special symbol delete it, but always return true, otherwise false.
-			get: (map, property, _) => (property === map.Class.symbol || property === map.Class.#privateSymbol) ? map : map.#super('get', property),
-																	// If the property isn't a special symbol return it, otherwise return the mirrored Map.
-			getOwnPropertyDescriptor: (map, property) => (property === map.Class.symbol || property === map.Class.#privateSymbol) ? ({configurable: false, enumerable: false, value: map, writable: false}) : ({configurable: true, enumerable: true, value: map.#super('get', property), writable: true}),
-																	// Same as get really, just more details.
-			getPrototypeOf: (_) => Object.getPrototypeOf({}),		// Returns a plain object prototype since it's pretending to be an object. I have no idea if this breaks something, but I'm pretty sure without it that console.log() failed.
-			has: (map, property) => (property === map.Class.symbol || property === map.Class.#privateSymbol) || map.#super('has', property),
-																	// If the property isn't a special symbol check that it exists else return true.
-			isExtensible: (_) => true,								// Always extensible.
-			ownKeys: (map) => [...map.#super('keys')],				// Returns map keys as property names.
-			preventExtensions: (_) => false,						// Prevents making the object non-extensible.
-			set: (map, property, value, _) => !(property === map.Class.symbol || property === map.Class.#privateSymbol) && !!map.#super('set', property, value),
-																	// If the property isn't a special symbol set it.
-			setPrototypeOf: (_, __) => false,						// Disables prototype setting, I don't want to know what that would do to this thing.
+	#proxies = [];
+	get proxy() { return this.#proxies.at(-1); }
+	set proxy(proxy) {
+		if(proxy === undefined) { // Removes a proxy off the stack if the input is undefined.
+			if(this.#proxies.length > 1) { // Don't do it if we just have the starting proxy.
+				this.#proxies.pop();
+			}
+		}else if(this.Class.#symbolList.every((symbol) => proxy[symbol] !== this) || proxy[Symbol()] === this) {
+			// Check that it returns the map if it passes the special symbols, but fails if any symbol causes a return of the map.
+			throw new Error("Proxy cannot be confirmed to reference the correct backer."); 
+		}else {
+			this.#proxies.push(proxy);
+		}
+	}
+	get map() { return this.proxy[this.Class.publicSymbol]; } // Returns a direct reference to the backing map.
+
+
+	#reservedKeysStore = this.Class.#getAllKeys(this); // Initialize with keys.
+	// Call the helper function on ourself on demand. Filter out the initial keys.
+	get definedKeys() { return this.Class.#getAllKeys(this).filter((key) => !(this.reservdKeys).includes(key)); }
+	get reservdKeys() { return this.#reservedKeysStore; } // Return the stored initial keys.
+	// Helper to check if a property is a specified kind of key.
+	// Defaults to checking if it's any explicitly defined key, but if any kind is specified it only checks the specified kinds.
+	#propertyCheck(property, {special, defined, reservd} = {special: true, reservd: true, defined: true}) {
+		const specialKeys = special && this.Class.#symbolList.includes(property);
+		const reservdKeys = reservd &&       this.reservdKeys.includes(property);
+		const definedKeys = defined &&       this.definedKeys.includes(property);
+		return specialKeys || reservdKeys || definedKeys;
+	}
+
+	constructor(iterable) {
+		super(); // Can't call set until after the next "line" and super(iterable) does.
+		this.proxy = new Proxy(this, {
+			defineProperty(map, property, _) { // Allow defining of properties if they aren't reserved or special.
+				if(map.#propertyCheck(property, {reservd: true, special: true})) throw new Error(`Cannot define reserved property: {${property}}`);
+				return Reflect.defineProperty(...arguments);
+			},
+			deleteProperty(map, property) { // Allow deleting of "properties" if they aren't reserved or special.
+				if(map.#propertyCheck(property, {reservd: true, special: true})) throw new Error(`Cannot delete reserved property: {${property}}`);
+				map.#super('delete', property);
+				return true;
+			},
+			get(map, property, _) {
+				if(map.#propertyCheck(property, {special: true})) return map; // If special, return the backing map.
+				if(map.#propertyCheck(property, {defined: true, reservd: true})) return map?.[property]; // If defined or reserved return off of the backing map.
+				return map.#super('get', property);
+			},
+			has(map, property) {
+				if(map.#propertyCheck(property, {special: true})) return true; // If special, return if the backing map exists... it does.
+				if(map.#propertyCheck(property, {defined: true, reservd: true})) return property in map; // If defined or reserved check off of the backing map.
+				return map.#super('has', property);
+			},
+			set(map, property, value, _) { // Allow setting of "properties" if they aren't reserved or special.
+				if(map.#propertyCheck(property, {reservd: true, special: true})) throw new Error(`Cannot set reserved property: {${property}}`);
+				return !!map.#super('set', property, value);
+			},
+			getOwnPropertyDescriptor(map, property) { // If special or reserved, return a dummy descriptor.
+				if(map.#propertyCheck(property, {reservd: true, special: true})) return {configurable: false, enumerable: false, value: map, writable: false};
+				if(map.#propertyCheck(property, {defined: true})) return Reflect.getOwnPropertyDescriptor(...arguments); // If defined, can just reflect.
+				// Else return a different dummy descriptor if the property exists.
+				if(map.#super('has', property)) return {configurable: true, enumerable: true, value: map.#super('get', property), writable: true};
+			},
+			ownKeys(map) { return [...map.#super('keys')]; },
+			getPrototypeOf(map) { return Object.getPrototypeOf(map) }, // Matches default IIRC, but I don't trust it.
+			// Don't feel like handling the logic for the rest of these. Besides, in most cases using these would defeat the purpose of this map.
+			isExtensible: (_) => true,
+			preventExtensions: (_) => false,
+			setPrototypeOf: (_, __) => false,
 		});
+		if(iterable && typeof iterable === 'object') { // Added object check to allow converting the result of JSON.parse on this class.
+			iterable = Object.entries(iterable)
+		}
+		for(const entry of iterable) { // set iterable here since it couldn't be done before.
+			this.set(...entry);
+		}
+		return this.proxy; // Return a proxy instead of the map. This way it always has the traps in theory.
 	}
 
-	#super(func, ...args) {											// Helper to call super methods dynamically. I'm pretty sure that I can't always call super from the handler (If I didn't use arrows) so I need a helper and I'm not writting a dozen different ones.
-		return typeof super[func] === 'function' ? super[func](...args) : super[func];
+	#super(func, ...args) { // Private helper function to make super calls/gets.
+		if(typeof super[func] === 'function') return super[func](...args);
+		return super[func];
 	}
-	clear() {														// Clears all entries iteratively, not sure it's the best way.
+	clear() { // Clears all entries iteratively. I'm not sure it's the best way and certainly not happy with it.
 		this.forEach((_, key, map) => map.delete(key));
 	}
-	delete(key) {													// Deletes a key if it exists, since delete always returns true pretty much.
-		return key !== undefined && key !== null && this.has(key) && delete this.mirror[key];
+	delete(key) { // Since delete always returns true pretty much, check manually first.
+		if(this.map.#propertyCheck(key)) return false; // Don't allow use of Map function on non-map keys.
+		return key !== undefined && key !== null && this.has(key) && delete this.proxy[key];
 	}
-	entries() {														// Converts the Object.entries return Array into an Array iterator, it's close enough.
-		return Object.entries(this.mirror)[Symbol.iterator]();
+	entries() { // Pass it through a new map to get a mapiterator. (capitalize? IDK.)
+		return (new Map(Object.entries(this.proxy))).entries();
 	}
-	forEach(callbackFn, thisArg = undefined) {						// Uses the entries method to make an array and iterate over everything.
+	forEach(callbackFn, thisArg = undefined) { // I might not need this one, but that depends on how the super calls the callback, so it's here.
 		[...this.entries()].forEach(([key, value]) => callbackFn(value, key, this, thisArg));
 	}
 	get(key) {
-		return this.mirror[key];
+		if(this.map.#propertyCheck(key)) return false; // Don't allow use of Map function on non-map keys.
+		return this.proxy[key];
 	}
 	has(key) {
-		return key in this.mirror;
+		if(this.map.#propertyCheck(key)) return false; // Don't allow use of Map function on non-map keys.
+		return key in this.proxy; // This is where I finally learn 'in' is a keyword AND committed it to memory.
 	}
 	keys() {
-		return Object.keys(this.mirror);
+		return Object.keys(this.proxy);
 	}
-	set(key, value) {												// Sets value for key, and does funky boolean stuff to always return this in one line rather than value.
-		return (this.mirror[key] = value) && this || this;
+	set(key, value) {
+		if(this.map.#propertyCheck(key)) return false; // Don't allow use of Map function on non-map keys.
+		this.proxy[key] = value;
+		return this; // Return this since object syntax returns value.
 	}
 	values() {
-		return Object.values(this.mirror);
+		return Object.values(this.proxy);
 	}
-	[Symbol.iterator]() {											// Docs say this is practically an alias so...
+	[Symbol.iterator]() { // Docs say this is practically an alias so...
 		return this.entries();
 	}
 
-	get size() {													// Nothing to do here since the object doesn't have an equivalent.
+	get size() { // Nothing special to do here since the object doesn't have an equivalent.
 		return super.size;
 	}
-	get [Symbol.toStringTag]() {									// Asked AI for boilerplate and it mentioned this so I made it look FANCY.
+	get [Symbol.toStringTag]() { // Asked AI for boilerplate and it mentioned this so I made it look FANCY.
 		return this.Class.name;
+	}
+	toString() { // Couldn't be bothered to do anything more. Does highlight that at least one benefit is that it allows a map to stringify.
+		return JSON.stringify(this);
 	}
 }
 
